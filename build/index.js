4bfb21b0e3e71db75f80ec9ec3d5f691b3183164
Brian Mullan <bmullan91@gmail.com>
2019-08-08 12:25:36 +0100

    Babel src

--- build/index.js
+++ build/index.js
@@ -21,25 +21,12 @@ import { stringToTokens } from './lib/util/stringToTokens';
 /**
  *
  */
-export {
-  applyStyle,
-  getUniqueID,
-  openUrl,
-  hasParents,
-  renderRules,
-  AstRenderer,
-  parser,
-  stringToTokens,
-  tokensToAST,
-  MarkdownIt,
-  PluginContainer,
-  blockPlugin,
-  styles,
-};
 
+export { applyStyle, getUniqueID, openUrl, hasParents, renderRules, AstRenderer, parser, stringToTokens, tokensToAST, MarkdownIt, PluginContainer, blockPlugin, styles };
 /**
  * react-native-markdown-renderer
  */
+
 export default class Markdown extends Component {
   constructor(props) {
     super(props);
@@ -47,13 +34,14 @@ export default class Markdown extends Component {
     this.renderer = null;
     this.markdownParser = null;
   }
-
   /**
    * Only when the copy changes will the markdown render again.
    * @param nextProps
    * @param nextState
    * @return {boolean}
    */
+
+
   shouldComponentUpdate(nextProps, nextState) {
     const copy = this.getCopyFromChildren(nextProps.children);
 
@@ -62,44 +50,41 @@ export default class Markdown extends Component {
       return true;
     }
 
-    if (
-      nextProps.renderer !== this.props.renderer ||
-      nextProps.style !== this.props.style ||
-      nextProps.plugins !== this.props.plugins ||
-      nextProps.rules !== this.props.rules ||
-      nextProps.markdownit !== this.props.markdownit
-    ) {
+    if (nextProps.renderer !== this.props.renderer || nextProps.style !== this.props.style || nextProps.plugins !== this.props.plugins || nextProps.rules !== this.props.rules || nextProps.markdownit !== this.props.markdownit) {
       return true;
     }
 
     return false;
   }
-
   /**
    *
    * @param props
    */
+
+
   updateSettings(props = this.props) {
-    const { renderer, rules, style, plugins, markdownit } = props;
+    const {
+      renderer,
+      rules,
+      style,
+      plugins,
+      markdownit
+    } = props;
 
     if (renderer && rules) {
-      console.warn(
-        'react-native-markdown-renderer you are using renderer and rules at the same time. This is not possible, props.rules is ignored'
-      );
+      console.warn('react-native-markdown-renderer you are using renderer and rules at the same time. This is not possible, props.rules is ignored');
     }
 
     if (renderer && style) {
-      console.warn(
-        'react-native-markdown-renderer you are using renderer and style at the same time. This is not possible, props.style is ignored'
-      );
-    }
+      console.warn('react-native-markdown-renderer you are using renderer and style at the same time. This is not possible, props.style is ignored');
+    } // these checks are here to prevent extra overhead.
 
-    // these checks are here to prevent extra overhead.
+
     if (renderer) {
       if (typeof renderer === 'function') {
         if (!this.renderer || this.renderer.render !== renderer) {
           this.renderer = {
-            render: renderer,
+            render: renderer
           };
         }
       } else if (renderer instanceof AstRenderer) {
@@ -111,21 +96,17 @@ export default class Markdown extends Component {
       }
     } else {
       if (!this.renderer || this.props.renderer || this.props.rules !== rules || this.props.style !== style) {
-        this.renderer = new AstRenderer(
-          {
-            ...renderRules,
-            ...(rules || {}),
-          },
-          {
-            ...styles,
-            ...style,
-          }
-        );
+        this.renderer = new AstRenderer({ ...renderRules,
+          ...(rules || {})
+        }, { ...styles,
+          ...style
+        });
       }
     }
 
     if (!this.markdownParser || this.props.markdownit !== markdownit || plugins !== this.props.plugins) {
       let md = markdownit;
+
       if (plugins && plugins.length > 0) {
         plugins.forEach(plugin => {
           md = md.use.apply(md, plugin.toArray());
@@ -135,42 +116,45 @@ export default class Markdown extends Component {
       this.markdownParser = md;
     }
   }
-
   /**
    *
    */
+
+
   componentWillMount() {
     this.updateSettings(this.props);
   }
-
   /**
    *
    * @param nextProps
    */
+
+
   componentWillReceiveProps(nextProps) {
     this.updateSettings(nextProps);
   }
-
   /**
    *
    * @param children
    * @return {string}
    */
+
+
   getCopyFromChildren(children = this.props.children) {
     return children instanceof Array ? children.join('') : children;
   }
-
   /**
    *
    * @return {View}
    */
+
+
   render() {
-    const copy = (this.copy = this.getCopyFromChildren());
+    const copy = this.copy = this.getCopyFromChildren();
     return parser(copy, this.renderer.render, this.markdownParser);
   }
+
 }
-
-
 Markdown.propTypes = {
   children: PropTypes.node.isRequired,
   renderer: PropTypes.oneOfType([PropTypes.func, PropTypes.instanceOf(AstRenderer)]),
@@ -187,27 +171,21 @@ Markdown.propTypes = {
     }
 
     if (typeof prop !== 'object') {
-      return new Error(
-        `Invalid prop \`${propName}\` supplied to \`${componentName}\`. Must be of shape {[index:string]:function} `
-      );
+      return new Error(`Invalid prop \`${propName}\` supplied to \`${componentName}\`. Must be of shape {[index:string]:function} `);
     } else if (invalidProps.length > 0) {
-      return new Error(
-        `Invalid prop \`${propName}\` supplied to \`${componentName}\`. These ` +
-          `props are not of type function \`${invalidProps.join(', ')}\` `
-      );
+      return new Error(`Invalid prop \`${propName}\` supplied to \`${componentName}\`. These ` + `props are not of type function \`${invalidProps.join(', ')}\` `);
     }
   },
   markdownit: PropTypes.instanceOf(MarkdownIt),
   plugins: PropTypes.arrayOf(PropTypes.instanceOf(PluginContainer)),
-  style: PropTypes.any,
+  style: PropTypes.any
 };
-
 Markdown.defaultProps = {
   renderer: null,
   rules: null,
   plugins: [],
   style: null,
   markdownit: MarkdownIt({
-    typographer: true,
-  }),
+    typographer: true
+  })
 };
